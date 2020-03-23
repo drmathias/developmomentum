7a79f376fe779cf198fe71e453bf8b33c9b2d991
Adam Shirt <adam.shirt@developmomentum.com>
2020-03-23 22:52:52 +0000

    Prepare construction site

---
+++ src/Program.cs
@@ -0,0 +1,15 @@
+﻿using Statiq.App;
+using Statiq.Web;
+using System.Threading.Tasks;
+
+namespace Blog
+{
+    class Program
+    {
+        private static async Task<int> Main(string[] args) =>
+            await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .RunAsync();
+    }
+}
